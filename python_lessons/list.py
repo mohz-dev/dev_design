3ab51c770a3638fa90d30705379379e64d1fa051
mohz-dev <bbg.zubairu@gmail.com>
2026-01-21 15:11:01 +0000

    Delete python_lessons/list.py

--- python_lessons/list.py
+++
@@ -1,9 +0,0 @@
-# List are containers used to store different data types
-mixed = ["Moh", 1, "Mama", 2]
-print(mixed)
-
-# Accessing items in a list
-names = ["Moh", "Hally", "Gospel", "Elvis", "Kaouther"]
-print(names[0:2])
-print(names[::2])  # prints elements in the list with a step of 2,
-print(names[-1])  # prints the last item on the list
